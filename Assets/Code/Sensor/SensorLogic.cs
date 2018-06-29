623148a45e99cf3ca83d046761a49d4cd2886347
ppecar <pp1508@student.uni-lj.si>
2018-06-30 01:02:57 +0200

    General structure of the first test level, simple movement, roomba, sensor, ai scripts

---
+++ Assets/Code/Sensor/SensorLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// ir sensor should work up to 15cm ingame, need to figure out dimensions for this
+
+public class SensorLogic : MonoBehaviour {
+
+    public RaycastHit IRRaycastHit;
+    private float RayDistance = 0.5f;
+
+    void Start() {
+    }
+
+    void Update() {
+        Debug.DrawRay(transform.position, transform.forward * RayDistance, Color.red);
+        IRSensorSimulation();
+    }
+    void IRSensorSimulation() {
+        Physics.Raycast(transform.position, transform.forward, out IRRaycastHit, RayDistance);
+        Debug.Log(IRRaycastHit.distance);
+    }
+}
