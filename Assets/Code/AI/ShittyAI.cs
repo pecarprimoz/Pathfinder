f988f5d1349209ac60a122b9f9413a0429bf4562
ppecar <pp1508@student.uni-lj.si>
2018-06-30 01:23:40 +0200

    UPDATE: Removed the comment

--- Assets/Code/AI/ShittyAI.cs
+++ Assets/Code/AI/ShittyAI.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShittyAI : MonoBehaviour {
-    // Just testing if i didint fuckup git
     public GameObject FrontSensorGameObject;
 
     private SensorLogic FrontSensorLogic;
